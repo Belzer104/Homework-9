faa5b1399942f5cd7993c5091017d1e5ac658b92
Belzer104 <shablovskyiillia@gmail.com>
2022-11-15 01:01:48 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,16 +1,27 @@
-# def handler():
+import functools
+
+def input_error(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except IndexError:    
+                print("Please start bot")
+    return wrapper
 
 
-
+@input_error
 def main():
     contact = dict()
     user_comand = input("Start bot: enter ('Hello, Hi, Start'): ")
+
     if user_comand.casefold() == "Hello".casefold() or user_comand.casefold() == "Hi".casefold() or user_comand.casefold() == "Start".casefold():
             print("How can I help you?")
     else:
         raise IndexError
-    while True:
 
+    while True:
         user_comand = input()
         if user_comand.casefold() == "exit".casefold() or user_comand.casefold() == "close".casefold() or user_comand.casefold() == "good bye".casefold():
             print("Good bye!")
