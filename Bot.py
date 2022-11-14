74859ca0470ba06a49f35b700f229b111173c2fa
Belzer104 <shablovskyiillia@gmail.com>
2022-11-15 00:38:13 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,9 +5,12 @@
 def main():
     contact = dict()
     user_comand = input("Start bot: enter ('Hello, Hi, Start'): ")
-    while True:
-        if user_comand.casefold() == "Hello".casefold() or user_comand.casefold() == "Hi".casefold() or user_comand.casefold() == "Start".casefold():
+    if user_comand.casefold() == "Hello".casefold() or user_comand.casefold() == "Hi".casefold() or user_comand.casefold() == "Start".casefold():
             print("How can I help you?")
+    else:
+        raise IndexError
+    while True:
+
         user_comand = input()
         if user_comand.casefold() == "exit".casefold() or user_comand.casefold() == "close".casefold() or user_comand.casefold() == "good bye".casefold():
             print("Good bye!")
