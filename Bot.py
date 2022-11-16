54ced83f9c1d51c3c2e3629dcce11ee9f5f349d4
Belzer104 <shablovskyiillia@gmail.com>
2022-11-16 19:53:23 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -10,6 +10,7 @@ def error_start(func):
                 print("Bot don't start")
     return wrapper
 
+
 def input_error(func):
     def inner(*args, **kwargs):
 
@@ -33,40 +34,68 @@ def input_error(func):
 contacts = {}
 
 def hello():
+
     return "How can I help you?"
+
 
 @input_error
 def add(comand):
-    if comand == 4:
+
+    if len(comand) == 4:
+
         fullname = f"{comand[1]} {comand[2]}"
         phone = comand[3]
         contacts[fullname] = phone
+
         return f"Contact {fullname} added"
+
     else:
+
         firstname = comand[1] 
         phone = comand[2]
         contacts[firstname] = phone
+
         return f"Contact {firstname} added"
     
     
 @input_error
 def change(comand):
-    firstname = comand[1]
-    phone = comand[2]
-    old_phone = contacts[firstname]
-    contacts[firstname] = phone
-    return f"User {firstname} heve change {old_phone}, new{phone}"
+
+    if len(comand) == 4:
+
+        fullname = f"{comand[1]} {comand[2]}"
+        phone = comand[3]
+        contacts[fullname] = phone
+        old_phone = contacts[fullname]
+
+        return f"User {fullname} heve change {old_phone}, new {phone}"
+    
+    else:
+
+        firstname = comand[1]
+        phone = comand[2]
+        old_phone = contacts[firstname]
+        contacts[firstname] = phone
+
+        return f"User {firstname} heve change {old_phone}, new {phone}"
+
 
 def phone(comand):
     pass
 
+
 def show_all():
+
     contact = ""
+
     for name, phone in contacts.items():
         contact += f"{name}: {phone}\n"
+
     return contact
 
+
 def close():
+
     print("Good bye!")
 
 
@@ -76,10 +105,13 @@ def main():
 
     if bot_start.casefold() == "Hello".casefold() or bot_start.casefold() == "Hi".casefold() or bot_start == "Start".casefold():
             print(hello())
+
     else:
+
         raise ImportError
 
     while True:
+
         user_comand = input("Comand to bot ('add, change, phone, show all'): ").casefold()
         comand = user_comand.split()
 
@@ -97,6 +129,5 @@ def main():
             break
         
         
-
 if __name__ == "__main__":
     main()
